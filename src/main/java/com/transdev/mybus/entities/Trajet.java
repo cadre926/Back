9522c918e5525fe4afad949e2290f1c1e0d0030c
cadres926 <assabyl4@gmail.com>
2023-03-31 15:41:29 +0000

    MAj Tests

--- src/main/java/com/transdev/mybus/entities/Trajet.java
+++ src/main/java/com/transdev/mybus/entities/Trajet.java
@@ -1,19 +1,15 @@
 package com.transdev.mybus.entities;
 
-import java.util.ArrayList;
-import java.util.LinkedList;
+ 
 import java.util.List;
 
-import javax.persistence.CascadeType;
 import javax.persistence.Entity;
-import javax.persistence.FetchType;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
-import javax.persistence.JoinColumn;
-import javax.persistence.JoinTable;
+ 
 import javax.persistence.ManyToMany;
-import javax.persistence.ManyToOne;
+ 
 
 import lombok.AllArgsConstructor;
 import lombok.Data;
